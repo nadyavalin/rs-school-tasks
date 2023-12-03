07fefab041ba1540e0e9d49ade464d10aefa1467
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2023-12-03 20:09:46 +0300

    feat: add burger menu in index.html and menu.html

--- coffee-house/assets/js/index.js
+++ coffee-house/assets/js/index.js
@@ -1,1 +1,40 @@
-console.log((8 / 2 + 5 - -3 / 2));
+// Burger-menu
+function addListenersForBurgerMenu() {
+  const header = document.querySelector(".header");
+  const body = document.querySelector(".body");
+      
+  document
+    .querySelector(".burger-menu__btn")
+    .addEventListener("click", () => {
+      setTimeout(() => {
+        header.classList.toggle("open");
+        if (header.classList.contains("open")) {
+          body.style.overflow = 'hidden';
+        } else {
+          body.style.overflow = 'auto';
+        }
+      }, 0);
+    });
+  
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      header.classList.remove("open");
+    }
+  });
+  
+  document.body.addEventListener("click", (event) => {
+    if (
+      !event.target.classList.contains("nav") &&
+      !event.target.closest(".burger-menu__btn") &&
+      header.classList.contains("open")
+    ) {
+      body.style.overflow = 'auto';
+      header.classList.remove("open");
+    }
+  });
+}
+
+  // DOMContentLoaded
+document.addEventListener("DOMContentLoaded", () => {
+    addListenersForBurgerMenu();
+});
