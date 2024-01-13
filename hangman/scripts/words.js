541a64b9cea5722cd54c6efde4e8742ed67269a6
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-01-13 14:39:22 +0300

    feat: add 10 words

--- hangman/scripts/words.js
+++ hangman/scripts/words.js
@@ -39,4 +39,44 @@ export default [
     word: "dinner",
     hint: "The main meal of the day, taken either around midday or in the evening.",
   },
+  {
+    word: "cat",
+    hint: "A small domesticated carnivorous mammal with soft fur.",
+  },
+  {
+    word: "bed",
+    hint: "A piece of furniture for sleep or rest.",
+  },
+  {
+    word: "fridge",
+    hint: "A cabinet with refrigeration device.",
+  },
+  {
+    word: "house",
+    hint: "A building for human habitation, especially one that is lived in by a family or small group of people.",
+  },
+  {
+    word: "lamp",
+    hint: "A device for giving light.",
+  },
+  {
+    word: "tiger",
+    hint: "A very large solitary cat with a yellow-brown coat striped with black.",
+  },
+  {
+    word: "lamp",
+    hint: "A very large solitary cat with a yellow-brown coat striped with black.",
+  },
+  {
+    word: "carpet",
+    hint: "A floor or stair covering made from thick woven fabric.",
+  },
+  {
+    word: "music",
+    hint: "Vocal or instrumental sounds combined in such a way as to produce beauty harmony.",
+  },
+  {
+    word: "table",
+    hint: "A piece of furniture with a flat top and for legs.",
+  },
 ];
