8987554a7719b39430c5454c6842cd4feeeadbbb
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-01-21 18:00:17 +0300

    feat: add hints to the playing area

--- nonograms/script.js
+++ nonograms/script.js
@@ -2,28 +2,72 @@
 // eslint-disable-next-line import/extensions
 import templates from "./templates.js";
 
-function generateGrid(rows, cols) {
-  const gridContainer = document.createElement("div");
-  gridContainer.classList.add("grid-container");
+// Общий контейнер
+const container = document.createElement("div");
+container.classList.add("container");
+document.body.append(container);
 
-  for (let i = 0; i < rows; i += 1) {
-    const row = document.createElement("div");
-    row.classList.add("grid-row");
-    for (let j = 0; j < cols; j += 1) {
-      const cell = document.createElement("div");
-      cell.classList.add("grid-cell");
-      row.append(cell);
+// Подсказки по горизонтали
+function generateHintsHorizontal(template) {
+  const hintsContainerHorizontal = document.createElement("div");
+  hintsContainerHorizontal.classList.add("hints-container-horizontal");
+
+  for (let i = 0; i < template.length; i += 1) {
+    let count = 0;
+    let hintsValue = "";
+    for (let j = 0; j < template[i].length; j += 1) {
+      if (template[i][j] === 1) {
+        count += 1;
+      } else if (count > 0) {
+        hintsValue += `${count} `;
+        count = 0;
+      }
     }
-    gridContainer.append(row);
+    if (count > 0) {
+      hintsValue += count;
+    }
+    const hints = document.createElement("div");
+    hints.classList.add("hint");
+    hints.textContent = hintsValue;
+    hintsContainerHorizontal.append(hints);
   }
-
-  document.body.append(gridContainer);
+  return hintsContainerHorizontal;
 }
 
-const currentTemplate = templates[0];
+// Подсказки по вертикали
+function generateHintsVertical(template) {
+  const hintsContainerVertical = document.createElement("div");
+  hintsContainerVertical.classList.add("hints-container-vertical");
 
-function fillGridWithTemplate(template) {
-  const gridCells = document.querySelectorAll(".grid-cell");
+  for (let i = 0; i < template.length; i += 1) {
+    let count = 0;
+    let hintValue = "";
+    for (let j = 0; j < template[i].length; j += 1) {
+      if (template[i][j] === 1) {
+        count += 1;
+      } else if (count > 0) {
+        hintValue += `${count} `;
+        count = 0;
+      }
+    }
+    if (count > 0) {
+      hintValue += count;
+    }
+
+    const hint = document.createElement("div");
+    hint.classList.add("hints");
+    hint.textContent = hintValue;
+    hintsContainerVertical.append(hint);
+  }
+  return hintsContainerVertical;
+}
+
+// Выбор шаблона
+const currentTemplate = templates[1];
+
+// Заполнение ячеек шаблоном
+function fillCellsWithTemplate(template) {
+  const gridCells = document.querySelectorAll(".cell");
   for (let i = 0; i < template.length; i += 1) {
     for (let j = 0; j < template[i].length; j += 1) {
       if (template[i][j] === 1) {
@@ -33,6 +77,25 @@ function fillGridWithTemplate(template) {
   }
 }
 
+// Генерация игрового поля
+function generatePlayingArea(rows, cols) {
+  const playingArea = document.createElement("div");
+  playingArea.classList.add("playing-area");
+
+  for (let i = 0; i < rows; i += 1) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < cols; j += 1) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      row.append(cell);
+    }
+    playingArea.append(row);
+  }
+  container.append(playingArea);
+}
+
+// Состояние ячеек
 function toggleCellState(cell) {
   if (!cell.classList.contains("hidden")) {
     cell.classList.toggle("crossed");
@@ -42,13 +105,16 @@ function toggleCellState(cell) {
   }
 }
 
+// Клик мышкой
 document.addEventListener("click", (event) => {
-  if (event.target.classList.contains("grid-cell")) {
+  if (event.target.classList.contains("cell")) {
     toggleCellState(event.target);
   }
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-  generateGrid(5, 5);
-  fillGridWithTemplate(currentTemplate);
+  container.append(generateHintsHorizontal(currentTemplate));
+  generatePlayingArea(5, 5);
+  container.append(generateHintsVertical(currentTemplate));
+  fillCellsWithTemplate(currentTemplate);
 });
