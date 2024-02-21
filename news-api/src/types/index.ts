53875a1d909977da4bed0d96d6cffac3a2abc495
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-02-21 23:10:14 +0300

    feat: change and add some styles, change file news from js to ts

--- news-api/src/types/index.ts
+++ news-api/src/types/index.ts
@@ -1,10 +1,14 @@
-export interface ProductDetails {
-  someField: string;
-  someField2: number;
+export interface NewsItem {
+  source: NestedItems;
+  title: string;
+  urlToImage: string;
+  author: string;
+  publishedAt: string;
+  description: string;
+  url: string;
 }
 
-export interface Product {
+export interface NestedItems {
   id: string;
   name: string;
-  details: ProductDetails;
 }
