2808445e0289f9abbe7c19e699fbcb054b865140
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-02-23 09:59:39 +0300

    feat: change file controller from js to ts, change tabWidth from 4 to 2 in files appLoader and controller

--- news-api/src/components/controller/appLoader.ts
+++ news-api/src/components/controller/appLoader.ts
@@ -1,11 +1,11 @@
 import Loader from './loader';
 
 class AppLoader extends Loader {
-    constructor() {
-        super(String(process.env.API_URL), {
-            apiKey: String(process.env.API_KEY),
-        });
-    }
+  constructor() {
+    super(String(process.env.API_URL), {
+      apiKey: String(process.env.API_KEY),
+    });
+  }
 }
 
 export default AppLoader;
