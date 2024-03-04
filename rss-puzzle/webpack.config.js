450899e3402b315406c33f332df1a4e5b546a993
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-03-04 22:59:22 +0300

    chore: implement configuration files

---
+++ rss-puzzle/webpack.config.js
@@ -0,0 +1,10 @@
+import path from "path";
+
+export default {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
