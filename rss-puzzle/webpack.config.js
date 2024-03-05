7af8d11979d4c079cbde080ea7e042c9da43c6fd
Nadezhda Tkachuk <125882223+nadyavalin@users.noreply.github.com>
2024-03-05 08:26:07 -0800

    fix: RSS-PZ-00 Implement Configuration Files
    * fix: correct code and add proper files for webpack
    
    * fix: correct file pre-push for the correct git push
    
    * fix: correct file pre-push for the correct git push next time

--- rss-puzzle/webpack.config.js
+++ rss-puzzle/webpack.config.js
@@ -1,10 +1,52 @@
-import path from "path";
+const path = require("path");
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const DotenvWebpackPlugin = require("dotenv-webpack");
+const EslingPlugin = require("eslint-webpack-plugin");
 
-export default {
+const baseConfig = {
+  entry: path.resolve(__dirname, "./src/index"),
   mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.ts$/i,
+        use: "ts-loader",
+      },
+      {
+        test: /\.html$/,
+        use: "html-loader",
+      },
+    ],
   },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "./dist"),
+  },
+  plugins: [
+    new DotenvWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/index.html"),
+      filename: "index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new EslingPlugin({ extensions: "ts" }),
+  ],
+};
+
+module.exports = ({ mode }) => {
+  const isProductionMode = mode === "prod";
+  const envConfig = isProductionMode
+    ? require("./webpack.prod.config")
+    : require("./webpack.dev.config");
+
+  return merge(baseConfig, envConfig);
 };
