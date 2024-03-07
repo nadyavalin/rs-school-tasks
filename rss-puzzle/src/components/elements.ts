0557a745b64a24f050603fc629979707880b92d1
Nadezhda Tkachuk <125882223+nadyavalin@users.noreply.github.com>
2024-03-07 12:43:38 -0900

    feat: RSS-PZ-02 Implement Validation Input Fields
    * feat: implement checking inputs for English letters and hyphen
    
    * feat: implement validation for inputs
    
    * feat: add crosses to the error messages

--- rss-puzzle/src/components/elements.ts
+++ rss-puzzle/src/components/elements.ts
@@ -1,8 +1,13 @@
-export function createInput(id: string, placeholder: string) {
+export function createInput(
+  id: string,
+  className: string,
+  placeholder: string,
+) {
   const input = document.createElement("input");
   input.type = "text";
   input.id = id;
   input.name = id;
+  input.classList.add(className);
   input.placeholder = placeholder;
   input.setAttribute("required", "true");
   return input;
@@ -17,4 +22,11 @@ export function createButton(text: string) {
   button.classList.add("disabled");
   button.textContent = text;
   return button;
+}
+
+export function createErrorMessage(text: string) {
+  const errorMessage = document.createElement("p");
+  errorMessage.classList.add("error-message");
+  errorMessage.textContent = text;
+  return errorMessage;
 }
--- rss-puzzle/src/components/loginForm.ts
+++ rss-puzzle/src/components/loginForm.ts
@@ -1,25 +1,74 @@
-import { createInput, createButton } from "./elements";
+import { createInput, createButton, createErrorMessage } from "./elements";
+
+const firstNamePattern: RegExp = /^[A-Z][-a-z]{2,}$/;
+const surnamePattern: RegExp = /^[A-Z][-a-z]{3,}$/;
 
 const form = document.createElement("form");
-const inputFirstName = createInput("fname", "First Name");
-const inputSurname = createInput("sname", "Surname");
+const inputFirstName = createInput("fname", "input", "First Name");
+const errorMsgForFirstName = createErrorMessage(
+  "❌ Your first name must be more than 3 characters in English with a capital letter.",
+);
+const inputSurname = createInput("sname", "input", "Surname");
+const errorMsgForSurname = createErrorMessage(
+  "❌ Your surname must be more than 4 characters in English with a capital letter.",
+);
 const buttonLogin = createButton("Login");
 
-form.append(inputFirstName, inputSurname, buttonLogin);
+form.append(
+  inputFirstName,
+  errorMsgForFirstName,
+  inputSurname,
+  errorMsgForSurname,
+  buttonLogin,
+);
 
-function checkLoginInputs(): boolean {
-  const loginValue = inputFirstName.value.trim();
-  const passwordLogValue = inputSurname.value.trim();
-  return loginValue !== "" && passwordLogValue !== "";
+function isLoginInputsEmpty(): boolean {
+  const firstNameValue = inputFirstName.value.trim();
+  const surnameValue = inputSurname.value.trim();
+  return firstNameValue !== "" && surnameValue !== "";
+}
+
+function isValidInput(inputValue: string, pattern: RegExp): boolean {
+  return pattern.test(inputValue);
 }
 
 function updateButtonLoginState(): void {
-  const isValid = checkLoginInputs();
-  buttonLogin.disabled = !isValid;
-  buttonLogin.classList.toggle("disabled", !isValid);
+  const isValid = isLoginInputsEmpty();
+  if (
+    isValidInput(inputFirstName.value, firstNamePattern) &&
+    isValidInput(inputSurname.value, surnamePattern)
+  ) {
+    buttonLogin.disabled = !isValid;
+    buttonLogin.classList.toggle("disabled", !isValid);
+  }
 }
 
-inputFirstName.addEventListener("input", updateButtonLoginState);
-inputSurname.addEventListener("input", updateButtonLoginState);
+form.addEventListener("change", updateButtonLoginState);
+inputFirstName.addEventListener("blur", () => {
+  if (!isValidInput(inputFirstName.value, firstNamePattern)) {
+    inputFirstName.classList.add("invalid");
+  } else {
+    inputFirstName.classList.remove("invalid");
+  }
+  updateButtonLoginState();
+});
+
+inputSurname.addEventListener("blur", () => {
+  if (!isValidInput(inputSurname.value, surnamePattern)) {
+    inputSurname.classList.add("invalid");
+    inputSurname.classList.add("cross-icon");
+  } else {
+    inputSurname.classList.remove("invalid");
+  }
+  updateButtonLoginState();
+});
+
+inputFirstName.addEventListener("focus", () => {
+  inputFirstName.classList.remove("invalid");
+});
+
+inputSurname.addEventListener("focus", () => {
+  inputSurname.classList.remove("invalid");
+});
 
 export default form;
