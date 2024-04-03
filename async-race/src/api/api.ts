74a56fa7af592a8e3b6759bb3ffe23d9e89820e9
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-04-03 15:54:39 +0300

    refactor: put the server url into a global variable

--- async-race/src/api/api.ts
+++ async-race/src/api/api.ts
@@ -6,15 +6,17 @@ import {
   CarsStatus,
 } from "../types/interfaces";
 
+const BASE_URL = "http://127.0.0.1:3000";
+
 export async function getCars() {
-  const apiURLGarage = "http://127.0.0.1:3000/garage";
+  const apiURLGarage = `${BASE_URL}/garage`;
   const response = await fetch(apiURLGarage);
   const cars = await response.json();
   return cars;
 }
 
 export async function getWinners() {
-  const apiURLGarage = "http://127.0.0.1:3000/winners";
+  const apiURLGarage = `${BASE_URL}/winners`;
   const response = await fetch(apiURLGarage);
   const winners = await response.json();
   return winners;
@@ -22,7 +24,7 @@ export async function getWinners() {
 
 export async function getCarsPerPage(page: string): Promise<CarsResponse> {
   const response = await fetch(
-    `http://127.0.0.1:3000/garage?${new URLSearchParams({
+    `${BASE_URL}/garage?${new URLSearchParams({
       _page: page,
       _limit: "7",
     })}`,
@@ -42,7 +44,7 @@ export async function getCarsPerPage(page: string): Promise<CarsResponse> {
 }
 
 export async function getWinnersPerPage(id: number): Promise<WinnerCars> {
-  const response = await fetch(`http://127.0.0.1:3000/winners/${id}`);
+  const response = await fetch(`${BASE_URL}/winners/${id}`);
 
   if (!response.ok) {
     throw new Error("Network response was not ok");
@@ -57,7 +59,7 @@ export async function getWinnersPerPage(id: number): Promise<WinnerCars> {
 }
 
 export async function createNewCarInGarage(carData: NewCar): Promise<Car> {
-  const response = await fetch("http://127.0.0.1:3000/garage", {
+  const response = await fetch(`${BASE_URL}/garage`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -69,7 +71,7 @@ export async function createNewCarInGarage(carData: NewCar): Promise<Car> {
 
 export async function updateCarAttributes(carData: Car): Promise<Car> {
   const { id } = carData;
-  const response = await fetch(`http://127.0.0.1:3000/garage/${id}`, {
+  const response = await fetch(`${BASE_URL}/garage/${id}`, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
@@ -80,7 +82,7 @@ export async function updateCarAttributes(carData: Car): Promise<Car> {
 }
 
 export async function deleteCarFromGarage(id: number): Promise<void> {
-  await fetch(`http://127.0.0.1:3000/garage/${id}`, {
+  await fetch(`${BASE_URL}/garage/${id}`, {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json",
@@ -93,12 +95,9 @@ export async function controlCarEngine(
   id: number,
   status: boolean,
 ): Promise<CarsStatus> {
-  const response = await fetch(
-    `http://127.0.0.1:3000/engine?id=${id}&status=${status}`,
-    {
-      method: "PATCH",
-    },
-  );
+  const response = await fetch(`${BASE_URL}/engine?id=${id}&status=${status}`, {
+    method: "PATCH",
+  });
   return response.json();
 }
 
