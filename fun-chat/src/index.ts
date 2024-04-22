10df72ce16c6bde2c336dc161ed6ac1d3b41de2f
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-04-22 23:29:38 +0300

    fix: fix transition from info page into login form or chat depend on user is logined

--- fun-chat/src/index.ts
+++ fun-chat/src/index.ts
@@ -3,8 +3,10 @@ import "./pages/info.css";
 import "./pages/loginForm.css";
 import "./pages/chat.css";
 
+import { state } from "./store/state";
 import { infoButton, formArea } from "./pages/loginForm";
 import { backButton, infoArea } from "./pages/info";
+import { footer, header, main } from "./pages/chat";
 
 document.body.append(formArea);
 document.body.classList.add("body-chat");
@@ -16,5 +18,9 @@ infoButton.addEventListener("click", () => {
 
 backButton.addEventListener("click", () => {
   document.body.removeChild(infoArea);
-  formArea.classList.remove("form_hide");
+  if (!state.login) {
+    formArea.classList.remove("form_hide");
+  } else {
+    document.body.append(header, main, footer);
+  }
 });
